0106bb10aea92372f80b5e015c9526c2fa9f2082
gnidolin <gnidolin@mail.ru>
2023-10-10 17:26:53 +0300

    fixed

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import App from './App';
 
 test('renders greeting message', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Святухин Д.А.error/i);
+  const linkElement = screen.getByText(/Святухин Д.А./i);
   // const linkElement = screen.getByText(/Это тестовый проект/i);
   expect(linkElement).toBeInTheDocument();
 });
